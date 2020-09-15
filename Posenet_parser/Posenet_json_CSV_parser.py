843a449a3629588ee276b3045162eb6a5aa03483
Daniel Jan Szczebiot <szczebiot.daniel@gmail.com>
2020-09-15 13:36:49 +0200

    funktioniert!!

--- Posenet_parser/Posenet_json_CSV_parser.py
+++ Posenet_parser/Posenet_json_CSV_parser.py
@@ -5,30 +5,33 @@ import os
 
 
 def json_csv():
+    ##############################################################
+    ######### Hier muss nur der Ordner angepasst werden ##########
+    ##############################################################
     directory = 'c:/Users/szcze/Gitrepos/JsonParseCSV/json/json/'
+    ##############################################################
     for filename in os.listdir(directory):
         if filename.endswith(".json"):
             
             print(filename)
-
-            for n in range(20):
-                print(filename + ": test: "+ str(n))
-            """
-            df = pd.read_json(str(filename))
+            df = pd.read_json(directory+str(filename))
             header = ["part", "score", "position"]
-            n = 0
-            for n in df.count():
-                print("n: "+str(n))
-                for x in df.count(axis='index'):
+            #print(df.size)
+            for n in range(df.size):
+                #print(filename + ": "+ str(n))
+                for x in range(20):
+                    #print(filename + ": "+ str(x))
                     if int(x) == 6 or int(x) == 8 or int(x) == 10:
                         inputx = "input"+str(x)
                         dt=pd.DataFrame(df["data"][n]["xs"][str(inputx)])
                         base = os.path.splitext(filename)[0]
-                        if int(n) == 0 and int(x)== 6:
-                            dt.to_csv(str(base)+'.csv',mode= 'a', index = True, header=True , columns= header)
+                        print(inputx)
+                        if int(x)== 6 and int(n) == 0:
+                            dt.to_csv(directory+str(base)+'.csv', mode= 'a', index = True, header=True , columns= header)
                         else:
-                            dt.to_csv(str(base)+'.csv',mode= 'a', index = True, header=False , columns= header)
-                            """                   
+                            dt.to_csv(directory+str(base)+'.csv', mode= 'a', index = True, header=False , columns= header)
+        
+
 json_csv()
 
 
